f1571498f0cdbfc492604e7a373861888699cb8d
René Bergmann <mafh612@users.noreply.github.com>
2021-05-11 16:26:07 +0200

    Delete dist directory

--- dist/components/CardBody.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface CardBodyProps {
-    children: React.ReactNode;
-}
-declare const _default: ({ children }: CardBodyProps) => React.ReactElement;
-export default _default;
--- dist/components/List.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface ListProps {
-    children: React.ReactNode;
-}
-declare const _default: ({ children }: ListProps) => React.ReactElement;
-export default _default;
